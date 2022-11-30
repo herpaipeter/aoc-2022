360d8db709d89ff1dac462a2179666a58757d0b1
Herpai Péter <herpaip@datapress.hu>
2022-11-30 23:17:50 +0100

    Add day 1 structure

---
+++ src/test/java/hu/herpaipeter/day1/Day1Test.java
@@ -0,0 +1,13 @@
+package hu.herpaipeter.day1;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class Day1Test {
+
+    @Test
+    void empty_test() {
+    }
+}
