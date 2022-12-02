91adbde7faac94ec94c400d3161ac6482fe1aa60
Herpai Péter <herpaip@datapress.hu>
2022-12-02 21:38:41 +0100

    Refactor day 2 point counter algorithms

--- src/main/java/hu/herpaipeter/aoc2022/day02/RPSOutcomePointAlgorithm.java
+++ src/main/java/hu/herpaipeter/aoc2022/day02/RPSOutcomePointAlgorithm.java
@@ -1,47 +1,20 @@
 package hu.herpaipeter.aoc2022.day02;
 
+import java.util.Map;
+
 public class RPSOutcomePointAlgorithm implements RPSPointAlgorithm {
+
+    Map<String, Integer> player1Symbols = Map.of("A", 1, "B", 2, "C", 3);
+    Map<String, Integer> resultSymbols = Map.of("X", 0, "Y", 3, "Z", 6);
     private static final int P1_CHOICE = 0;
-    private static final int P2_CHOICE = 1;
-    private static final String P1_ROCK = "A";
-    private static final String P1_PAPER = "B";
-    private static final String P1_SCISSORS = "C";
-    private static final int ROCK_POINT = 1;
-    private static final int PAPER_POINT = 2;
-    private static final int SCISSORS_POINT = 3;
-    private static final int DRAW_POINT = 3;
-    private static final int LOSE_POINT = 0;
-    private static final int WIN_POINT = 6;
-    private static final String P2_LOSE = "X";
-    private static final String P2_DRAW = "Y";
-    private static final String P2_WIN = "Z";
+    private static final int RESULT = 1;
 
     @Override
     public int getPoints(String[] round) {
-        if (round[P1_CHOICE].equalsIgnoreCase(P1_ROCK)) {
-            if (round[P2_CHOICE].equalsIgnoreCase(P2_DRAW))
-                return DRAW_POINT + ROCK_POINT;
-            else if (round[P2_CHOICE].equalsIgnoreCase(P2_LOSE))
-                return LOSE_POINT + SCISSORS_POINT;
-            else if (round[P2_CHOICE].equalsIgnoreCase(P2_WIN))
-                return WIN_POINT + PAPER_POINT;
-        }
-        else if (round[P1_CHOICE].equalsIgnoreCase(P1_PAPER)) {
-            if (round[P2_CHOICE].equalsIgnoreCase(P2_DRAW))
-                return DRAW_POINT + PAPER_POINT;
-            else if (round[P2_CHOICE].equalsIgnoreCase(P2_LOSE))
-                return LOSE_POINT + ROCK_POINT;
-            else if (round[P2_CHOICE].equalsIgnoreCase(P2_WIN))
-                return WIN_POINT + SCISSORS_POINT;
-        }
-        else if (round[P1_CHOICE].equalsIgnoreCase(P1_SCISSORS)) {
-            if (round[P2_CHOICE].equalsIgnoreCase(P2_DRAW))
-                return DRAW_POINT + SCISSORS_POINT;
-            else if (round[P2_CHOICE].equalsIgnoreCase(P2_LOSE))
-                return LOSE_POINT + PAPER_POINT;
-            else if (round[P2_CHOICE].equalsIgnoreCase(P2_WIN))
-                return WIN_POINT + ROCK_POINT;
-        }
-        return 0;
+        int p1choice = player1Symbols.get(round[P1_CHOICE]);
+        int result = resultSymbols.get(round[RESULT]);
+        int p2choice = (p1choice + (int)Math.signum(result - 3)) % 3;
+        p2choice = p2choice == 0 ? 3 : p2choice;
+        return result + p2choice;
     }
 }
--- src/main/java/hu/herpaipeter/aoc2022/day02/RPSSimplePointAlgorithm.java
+++ src/main/java/hu/herpaipeter/aoc2022/day02/RPSSimplePointAlgorithm.java
@@ -1,46 +1,24 @@
 package hu.herpaipeter.aoc2022.day02;
 
+import java.util.Map;
+
 public class RPSSimplePointAlgorithm implements RPSPointAlgorithm {
 
+    Map<String, Integer> player1Symbols = Map.of("A", 1, "B", 2, "C", 3);
+    Map<String, Integer> player2Symbols = Map.of("X", 1, "Y", 2, "Z", 3);
 
     private static final int P1_CHOICE = 0;
     private static final int P2_CHOICE = 1;
-    private static final String P2_ROCK = "X";
-    private static final String P2_PAPER = "Y";
-    private static final String P2_SCISSOR = "Z";
-    private static final String P1_ROCK = "A";
-    private static final String P1_PAPER = "B";
-    private static final String P1_SCISSORS = "C";
-    private static final int ROCK_POINT = 1;
-    private static final int PAPER_POINT = 2;
-    private static final int SCISSORS_POINT = 3;
     private static final int DRAW_POINT = 3;
     private static final int WIN_POINT = 6;
 
     @Override
     public int getPoints(String[] round) {
-        int result = 0;
-        if (round[P2_CHOICE].equalsIgnoreCase(P2_ROCK)) {
-            result += ROCK_POINT;
-            if (round[P1_CHOICE].equalsIgnoreCase(P1_ROCK))
-                result += DRAW_POINT;
-            else if (round[P1_CHOICE].equalsIgnoreCase(P1_SCISSORS))
-                result += WIN_POINT;
-        }
-        else if (round[P2_CHOICE].equalsIgnoreCase(P2_PAPER)) {
-            result += PAPER_POINT;
-            if (round[P1_CHOICE].equalsIgnoreCase(P1_PAPER))
-                result += DRAW_POINT;
-            else if (round[P1_CHOICE].equalsIgnoreCase(P1_ROCK))
-                result += WIN_POINT;
-        }
-        else if (round[P2_CHOICE].equalsIgnoreCase(P2_SCISSOR)) {
-            result += SCISSORS_POINT;
-            if (round[P1_CHOICE].equalsIgnoreCase(P1_SCISSORS))
-                result += DRAW_POINT;
-            else if (round[P1_CHOICE].equalsIgnoreCase(P1_PAPER))
-                result += WIN_POINT;
-        }
+        int p1choice = player1Symbols.get(round[P1_CHOICE]);
+        int p2choice = player2Symbols.get(round[P2_CHOICE]);
+        int result = p2choice;
+        if ((p1choice) % 3 == p2choice - 1) result += WIN_POINT;
+        else if (p1choice == p2choice) result += DRAW_POINT;
         return result;
     }
 }
