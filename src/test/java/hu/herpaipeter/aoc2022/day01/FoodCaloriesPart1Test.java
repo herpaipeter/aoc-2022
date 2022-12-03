b5046ccf117a18cb537294506aa96193407b2727
Herpai Péter <herpaip@datapress.hu>
2022-12-03 10:09:24 +0100

    Refactor day 1 test with setUp func

--- src/test/java/hu/herpaipeter/aoc2022/day01/FoodCaloriesPart1Test.java
+++ src/test/java/hu/herpaipeter/aoc2022/day01/FoodCaloriesPart1Test.java
@@ -1,6 +1,7 @@
 package hu.herpaipeter.aoc2022.day01;
 
 import hu.herpaipeter.common.FileReader;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -12,45 +13,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class FoodCaloriesPart1Test {
 
+    FoodCalories foodCalories;
+
+    @BeforeEach
+    void setUp() {
+        foodCalories = new FoodCalories();
+    }
+
     @Test
     void empty_input_should_return_zero() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(0, foodCalories.max(List.of()));
     }
 
     @Test
     void one_element_should_return_same() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(1, foodCalories.max(List.of("1")));
     }
 
     @Test
     void two_elements_should_return_sum() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(3, foodCalories.max(List.of("1", "2")));
     }
 
     @Test
     void more_elements_should_return_sum() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(5063, foodCalories.max(List.of("1", "2", "10", "5050")));
     }
 
     @Test
     void separated_elements_should_return_the_greater() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(2, foodCalories.max(List.of("1", "", "2")));
     }
 
     @Test
     void separated_elements_should_return_the_greater_sum() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(4, foodCalories.max(List.of("1", "3", "", "1", "2")));
     }
 
     @Test
     void multi_elements_groups_should_return_the_greater_sum_independent_of_order() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(5003, foodCalories.max(List.of("1", "100", "3", "306", "", "1", "2", "5000", "", "1", "10", "33", "306", "1521")));
         assertEquals(5003, foodCalories.max(List.of("1", "2", "5000", "", "1", "100", "3", "306", "", "1", "10", "33", "306", "1521")));
     }
@@ -58,7 +59,6 @@ public class FoodCaloriesPart1Test {
     @Test
     void get_file_values() {
         List<String> calories = FileReader.readStringLines("src\\test\\java\\hu\\herpaipeter\\aoc2022\\day01\\input.txt");
-        FoodCalories foodCalories = new FoodCalories();
         System.out.println(foodCalories.max(calories));
     }
 }
--- src/test/java/hu/herpaipeter/aoc2022/day01/FoodCaloriesPart2Test.java
+++ src/test/java/hu/herpaipeter/aoc2022/day01/FoodCaloriesPart2Test.java
@@ -1,6 +1,7 @@
 package hu.herpaipeter.aoc2022.day01;
 
 import hu.herpaipeter.common.FileReader;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -11,55 +12,53 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class FoodCaloriesPart2Test {
+    FoodCalories foodCalories;
+
+    @BeforeEach
+    void setUp() {
+        foodCalories = new FoodCalories();
+    }
 
     @Test
     void empty_input_should_return_zero() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(0, foodCalories.sumOfMaxes(List.of(), 0));
     }
 
     @Test
     void one_element_should_return_same() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(1, foodCalories.sumOfMaxes(List.of("1"), 1));
     }
 
     @Test
     void more_elements_should_return_sum() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(5063, foodCalories.sumOfMaxes(List.of("1", "2", "10", "5050"), 1));
     }
 
     @Test
     void separated_elements_should_return_the_greater_with_maxcount_1() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(2, foodCalories.sumOfMaxes(List.of("1", "", "2"), 1));
     }
 
     @Test
     void two_separated_elements_should_return_the_sum_with_maxcount_2() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(3, foodCalories.sumOfMaxes(List.of("1", "", "2"), 2));
         assertEquals(3, foodCalories.sumOfMaxes(List.of("1", "", "2"), 3));
     }
 
     @Test
     void multi_elements_groups_should_return_the_all_sum_independent_of_order() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(7284, foodCalories.sumOfMaxes(List.of("1", "100", "3", "306", "", "1", "2", "5000", "", "1", "10", "33", "306", "1521"), 3));
         assertEquals(7284, foodCalories.sumOfMaxes(List.of("1", "2", "5000", "", "1", "100", "3", "306", "", "1", "10", "33", "306", "1521"), 3));
     }
 
     @Test
     void multi_elements_groups_should_return_only_the_first_3_max_sum() {
-        FoodCalories foodCalories = new FoodCalories();
         assertEquals(9, foodCalories.sumOfMaxes(List.of("1", "", "2", "", "3", "", "4"), 3));
     }
 
     @Test
     void get_file_values() {
         List<String> calories = FileReader.readStringLines("src\\test\\java\\hu\\herpaipeter\\aoc2022\\day01\\input.txt");
-        FoodCalories foodCalories = new FoodCalories();
         System.out.println(foodCalories.sumOfMaxes(calories, 3));
     }
 }
