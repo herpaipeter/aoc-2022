f448a97e37f18fb184e02ac61b7f0c6cf1a3d4dd
Herpai Péter <herpaip@datapress.hu>
2022-12-06 22:08:16 +0100

    Refactor day 6 magic numbers

--- src/main/java/hu/herpaipeter/aoc2022/day06/CommunicationProtocol.java
+++ src/main/java/hu/herpaipeter/aoc2022/day06/CommunicationProtocol.java
@@ -2,18 +2,21 @@ package hu.herpaipeter.aoc2022.day06;
 
 public class CommunicationProtocol {
 
+    private static final int MARKER_LENGTH = 4;
+    private static final int MESSAGE_LENGTH = 14;
+
     public int getMarkerStart(String datastream) {
-        for (int i = 0; i < datastream.length() - 3; i++) {
-            if (StringValidator.containsUniqueChars(datastream.substring(i, i + 4)))
-                return i + 4;
+        for (int i = 0; i < datastream.length() - MARKER_LENGTH + 1; i++) {
+            if (StringValidator.containsUniqueChars(datastream.substring(i, i + MARKER_LENGTH)))
+                return i + MARKER_LENGTH;
         }
         return -1;
     }
 
     public int getMessageStart(String datastream) {
-        for (int i = 0; i < datastream.length() - 13; i++) {
-            if (StringValidator.containsUniqueChars(datastream.substring(i, i + 14)))
-                return i + 14;
+        for (int i = 0; i < datastream.length() - MESSAGE_LENGTH + 1; i++) {
+            if (StringValidator.containsUniqueChars(datastream.substring(i, i + MESSAGE_LENGTH)))
+                return i + MESSAGE_LENGTH;
         }
         return -1;
     }
