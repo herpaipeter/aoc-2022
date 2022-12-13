75043a0b0295da454d0ef6db8d1231f086043925
Herpai Péter <herpaip@datapress.hu>
2022-12-13 23:18:25 +0100

    Delete day 12 unused classes

--- src/main/java/hu/herpaipeter/aoc2022/day12/MapPoint.java
+++
@@ -1,4 +0,0 @@
-package hu.herpaipeter.aoc2022.day12;
-
-public record MapPoint(int row, int col, char height) {
-}
--- src/test/java/hu/herpaipeter/aoc2022/day12/HillClimbingParserTest.java
+++ src/test/java/hu/herpaipeter/aoc2022/day12/HillClimbingParserTest.java
@@ -1,45 +1,14 @@
 package hu.herpaipeter.aoc2022.day12;
 
-import hu.herpaipeter.aoc2022.day11.Monkey;
-import hu.herpaipeter.aoc2022.day11.MonkeyKeepAway;
-import hu.herpaipeter.aoc2022.day11.MonkeyKeepAwayInputParser;
 import hu.herpaipeter.common.FileReader;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.IntStream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class HillClimbingParserTest {
-
-    @Test
-    void empty_string_should_return_empty_graph() {
-        HillClimbingParser parser = new HillClimbingParser();
-        assertEquals(0, parser.buildGrid(List.of()).Points().size());
-    }
-
-    @Test
-    void start_string_should_return_one_point() {
-        HillClimbingParser parser = new HillClimbingParser();
-        List<MapPoint> points = parser.buildGrid(List.of("S")).Points();
-        assertEquals(new MapPoint(0,0,'S'), points.get(0));
-    }
-
-//    @Test
-//    void two_chars_should_return_2_point_and_1_edge() {
-//        HillClimbingParser parser = new HillClimbingParser();
-//        ElevationGraph graph = parser.buildGrid(List.of("Sa"));
-//        MapPoint start = new MapPoint(0, 0, 'S');
-//        MapPoint pointA = new MapPoint(0, 0, 'S');
-//        assertEquals(2, graph.Points().size());
-//        assertEquals(List.of(start, pointA), graph.Points());
-//        assertEquals(List.of(new MapEdge(start, pointA)), graph.edges());
-//    }
 
     @Test
     void example_result() {
